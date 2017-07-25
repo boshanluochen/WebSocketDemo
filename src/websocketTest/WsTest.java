d09ae87a30dba7b2f1558a51462e5c13fb756e67
boshanluochen <boshanluochen.w@gmail.com>
2017-07-25 14:30:36 +0800

    web socket

---
+++ src/websocketTest/WsTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015-2017, Zhejiang QiZhi Technologies Co., Ltd.
+ * <http://www.shterm.com/>
+ */
+package websocketTest;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+
+//@ServerEndpoint(value = "/websocket")
+public class WsTest {
+
+	@OnMessage
+    public void onMessage(String message, Session session) 
+    	throws IOException, InterruptedException {
+		
+		// Print the client message for testing purposes
+		System.out.println("Received: " + message);
+		
+		// Send the first message to the client
+		session.getBasicRemote().sendText("This is the first server message");
+		
+		// Send 3 messages to the client every 5 seconds
+		int sentMessages = 0;
+		while(sentMessages < 3){
+			Thread.sleep(5000);
+			session.getBasicRemote().
+				sendText("This is an intermediate server message. Count: " 
+					+ sentMessages);
+			sentMessages++;
+		}
+		
+		// Send a final message to the client
+		session.getBasicRemote().sendText("This is the last server message");
+    }
+	
+	@OnOpen
+    public void onOpen () {
+        System.out.println("Client connected");
+    }
+
+    @OnClose
+    public void onClose () {
+    	System.out.println("Connection closed");
+    }
+}
